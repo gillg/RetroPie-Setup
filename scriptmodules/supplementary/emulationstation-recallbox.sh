28e9a9784bb437681bd93dce6cf27cdc4dad621a
Guillaume Gill <gillg02@petitchinois.net>
2017-02-06 18:38:05 +0100

    Use gillg repo for emulationstation

--- scriptmodules/supplementary/emulationstation-recallbox.sh
+++ scriptmodules/supplementary/emulationstation-recallbox.sh
@@ -14,11 +14,21 @@ rp_module_desc="EmulationStation from recallbox (translations/settings/debugs/et
 rp_module_section="exp"
 
 function depends_emulationstation-recallbox() {
-    depends_emulationstation
+    local depends=(
+        libboost-locale-dev libboost-system-dev libboost-filesystem-dev
+        libboost-date-time-dev libfreeimage-dev libfreetype6-dev libeigen3-dev
+        libcurl4-openssl-dev libasound2-dev cmake libsdl2-dev libsm-dev
+        libvlc-dev libvlccore-dev vlc-nox
+        libboost-regex-dev libboost-thread-dev libfreeimage-dev libsdl2-mixer-dev
+        libfreeimageplus-dev libfreeimage3-dbg libfreeimageplus3-dbg
+    )
+
+    isPlatform "x11" && depends+=(gnome-terminal)
+    getDepends "${depends[@]}"
 }
 
 function sources_emulationstation-recallbox() {
-    sources_emulationstation "https://github.com/recalbox/recalbox-emulationstation" "master"
+    sources_emulationstation "https://github.com/gillg/recalbox-emulationstation" "master"
 }
 
 function build_emulationstation-recallbox() {
